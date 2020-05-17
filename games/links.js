13b5028f3947b6769cc0956395170cdb18357c27
jordan <eclbetty97@gmail.com>
2020-05-17 17:57:15 +0800

    icon

---
+++ games/links.js
@@ -0,0 +1,20 @@
+/*
+1. Greatwall 🗿
+GreatWall Both SaveTime
+https://gw.goshrimp888.com/IGKiosk/playermgt/playerActions.aspx
+
+2. Suncity 🌇
+SunCity Both SaveTime
+https://clubsuncity.godolphin888.com/IGKiosk/playermgt/playerActions.aspx
+
+3. Joker 🃏
+Joker NewPlayer SaveTime
+http://www.super6699.net/Person/Add/Member?*
+
+Joker SetScore SaveTime
+http://www.super6699.net/Person/QuickTransfer?*
+
+4. Playboy2 🐰
+Playboy Both SaveTime
+https://pb128.goeel888.com/IGKiosk/playermgt/playerActions.aspx
+*/
